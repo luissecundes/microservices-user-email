bd7fb213f3f17e156876d43499238906001fe0b5
Luis <luissecundes@gmail.com>
2023-12-22 07:28:59 -0300

    Feito ORM da entidade User

---
+++ user/src/main/java/com/microservice/user/entities/UserEntity.java
@@ -0,0 +1,47 @@
+package com.microservice.user.entities;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class UserEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID userId;
+	private String name;
+	private String email;
+	public UUID getUserId() {
+		return userId;
+	}
+	public void setUserId(UUID userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+
+}
