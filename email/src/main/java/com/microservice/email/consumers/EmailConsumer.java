027d75abdeeb7496d2f3e98c0a75c928d4937366
Luis <luissecundes@gmail.com>
2023-12-26 11:58:57 -0300

    Finalizado o Consumer e iniciado o config do Producer

--- email/src/main/java/com/microservice/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/microservice/email/consumers/EmailConsumer.java
@@ -4,12 +4,14 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import com.microservice.email.dtos.EmailRecordDto;
+
 @Component
 public class EmailConsumer {
 	
 	@RabbitListener(queues = "${broker.queue.email.name}")
-	public void listenEmailQueue(@Payload String string) {
-		System.out.println(string);
+	public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
+		System.out.println(emailRecordDto.emailTo());
 	}
 
 }
